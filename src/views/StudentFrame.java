30b896f4d67166f5aa9e51a3f93d97626f6be817
aris <msuann25@gmail.com>
2026-01-13 00:32:42 +0800

    upd on stud frame and prepped layout

--- src/views/StudentFrame.java
+++ src/views/StudentFrame.java
@@ -1,16 +1,31 @@
 // fml 
 package views;
 
-import javax.management.openmbean.OpenDataException;
 import javax.swing.*;
 import java.awt.event.*;
+
 
 public class StudentFrame extends JFrame {
     public StudentFrame () {
         super ("Student");
-        setSize(400,600);
+        setSize(600,400);
         setLocationRelativeTo(null);
+        setVisible(true); //temp (just to check functionality)
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        // Has register and upload page
+        JTabbedPane studentPane = new JTabbedPane();
+        studentPane.setBounds(450,200,600,400);
+        this.add(studentPane);
+
+        JPanel panel1 = new JPanel();
+        JPanel panel2 = new JPanel();
+        
+        // Register and Upload tabs
+        studentPane.add("Registration", panel1);
+        studentPane.add("Upload", panel2);
+    }
+    
+    public static void main(String[] args) {
+        new StudentFrame();
     }
 }
