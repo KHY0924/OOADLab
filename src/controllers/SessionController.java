57217ff0c8151c188617c319f0a5694dffb1e0b9
creepmustacheguy <168409012+creepmustacheguy@users.noreply.github.com>
2026-01-16 14:07:34 +0800

    Update SessionController.java

--- src/controllers/SessionController.java
+++ src/controllers/SessionController.java
@@ -15,16 +15,6 @@ public class SessionController {
             e.printStackTrace();
         }
     }
-
-    public class DateAndTime {
-        public static void dateInput(int year, int month, int day){
-            LocalDate date = LocalDate.of(year, month, day);
-        }
-    
-        public static void timeInput(int hour, int minute){
-            LocalTime time = LocalTime.of(hour, minute);
-        }
-    }
     
     
 }
