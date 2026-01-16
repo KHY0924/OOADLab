93fbb0ece3baaa42c568f1012db89b50c2524237
creepmustacheguy <168409012+creepmustacheguy@users.noreply.github.com>
2026-01-16 14:13:44 +0800

    Remove nested DateAndTime class from Seminar
    Removed the nested DateAndTime class from Seminar.java.

--- src/models/Seminar.java
+++ src/models/Seminar.java
@@ -9,16 +9,6 @@ public class Seminar {
   private DateAndTime seminarDT;
   private String location;
 
-  public class DateAndTime {
-    public static void dateInput(int year, int month, int day){
-        LocalDate date = LocalDate.of(year, month, day);
-    }
-
-    public static void timeInput(int hour, int minute){
-        LocalTime time = LocalTime.of(hour, minute);
-    }
-  }
-
   public String getSeminarId() {
     return seminarId;
   }
