c71a1f5a331d0f53d9bfa1c45968c61b526525ab
creepmustacheguy <168409012+creepmustacheguy@users.noreply.github.com>
2026-01-16 17:22:21 +0800

    Refactor DateAndTime class to use instance variables

--- src/models/DateAndTime.java
+++ src/models/DateAndTime.java
@@ -1,11 +1,20 @@
-import models
+package models;
+import java.time.*;
 
 public class DateAndTime {
-    public static void dateInput(int year, int month, int day){
-        LocalDate date = LocalDate.of(year, month, day);
+    public LocalDate date;
+    public LocalTime time;
+
+    public DateAndTime(LocalDate date, LocalTime time) {
+        this.date = date;
+        this.time = time;
     }
 
-    public static void timeInput(int hour, int minute){
-        LocalTime time = LocalTime.of(hour, minute);
+    public static LocalDate dateInput(int year, int month, int day){
+        return LocalDate.of(year, month, day);
+    }
+
+    public static LocalTime timeInput(int hour, int minute){
+        return LocalTime.of(hour, minute);
     }
 }
