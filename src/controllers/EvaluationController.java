a22bf8bbcd8522267372334f7ce44d971248d42c
creepmustacheguy <168409012+creepmustacheguy@users.noreply.github.com>
2026-01-23 13:47:38 +0800

    Update EvaluationController.java

--- src/controllers/EvaluationController.java
+++ src/controllers/EvaluationController.java
@@ -37,12 +37,56 @@ public class EvaluationController {
     }
 
     public void viewEvaluation(String sessionID) {
-
+        List<Evaluation> evaluations = evaluationRepo.getEvaluationsBySessionId(sessionID);
+        System.out.println("Evaluations for Session: " + sessionID);
+        System.out.println("-----------------------------------");
+        for (Evaluation eval : evaluations) {
+            System.out.println("Submission: " + eval.getSubmissionId());
+            System.out.println("Evaluator: " + eval.getEvaluatorId());
+            System.out.println("Total Score: " + eval.getTotalScore());
+            System.out.println("Problem Clarity: " + eval.getProblemClarityScore());
+            System.out.println("Methodology: " + eval.getMethodologyScore());
+            System.out.println("Results: " + eval.getResultsScore());
+            System.out.println("Presentation: " + eval.getPresentationScore());
+            System.out.println("Comments: " + eval.getComments());
+            System.out.println("Created At: " + eval.getCreatedAt());
+            System.out.println("-----------------------------------");
+        }
     }
 
     public void generateAward(String submissionID) {
-        highestOralMarks(submissionID);
-        highestPosterMarks(submissionID);
-        highestOverallMarks(submissionID);
+        highestOralMarks();
+        highestPosterMarks();
+        highestOverallMarks();
+    }
+
+    private void highestOralMarks() {
+        int maxScore = evaluationRepo.getHighestScoreForSessionType("oral");
+        if (maxScore > 0) {
+            String submissionId = evaluationRepo.getSubmissionWithHighestScoreForType("oral", maxScore);
+            System.out.println("Highest Oral Marks: Submission " + submissionId + " with score " + maxScore);
+        } else {
+            System.out.println("No oral evaluations found.");
+        }
+    }
+
+    private void highestPosterMarks() {
+        int maxScore = evaluationRepo.getHighestScoreForSessionType("poster");
+        if (maxScore > 0) {
+            String submissionId = evaluationRepo.getSubmissionWithHighestScoreForType("poster", maxScore);
+            System.out.println("Highest Poster Marks: Submission " + submissionId + " with score " + maxScore);
+        } else {
+            System.out.println("No poster evaluations found.");
+        }
+    }
+
+    private void highestOverallMarks() {
+        int maxScore = evaluationRepo.getHighestOverallScore();
+        if (maxScore > 0) {
+            String submissionId = evaluationRepo.getSubmissionWithHighestOverallScore(maxScore);
+            System.out.println("Highest Overall Marks: Submission " + submissionId + " with score " + maxScore);
+        } else {
+            System.out.println("No evaluations found.");
+        }
     }
 }
